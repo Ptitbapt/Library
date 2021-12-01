0e47f6cca45072c373108835a79e51bd8c475383
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2021-12-01 21:55:26 +0100

    DecToHex finish

--- src/main/java/com/coding/library/Model/AlgoTest.java
+++ src/main/java/com/coding/library/Model/AlgoTest.java
@@ -3,27 +3,21 @@ import java.util.*;
 
 public class AlgoTest{
 
-
-    public static String toHex(){
-
-        Scanner decScan = new Scanner(System.in);
-
-        int decimal;
-        System.out.println("Enter the decimal");
-        decimal = decScan.nextInt();
-        int tmp;
+    
+    public static String toHex(int decimal){
+        int rem;
         String hex="";
         char hexchars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         while(decimal>0)
         {
-            tmp=decimal%16;
-            hex=hexchars[tmp]+hex;
+            rem=decimal%16;
+            hex=hexchars[rem]+hex;
             decimal = decimal/16;
-
         }
-
         return hex;
     }
     public static void main(String args[]){
-       toHex();
+        System.out.println("Hexadecimal of 10 is: "+toHex(45));
+        System.out.println("Hexadecimal of 15 is: "+toHex(15));
+        System.out.println("Hexadecimal of 289 is: "+toHex(289));
     }}
