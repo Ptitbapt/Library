1c65c85ab076d13bfa354d04aa3deed52c31b94e
Lucas Goncalves <lucasgoncalves@free.fr>
2021-12-03 10:31:58 +0100

    TEST

--- src/main/java/com/coding/library/controller/MainController.java
+++ src/main/java/com/coding/library/controller/MainController.java
@@ -1,57 +1,95 @@
-package com.coding.library.controller;
+package com.coding.library.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import com.coding.library.Controller.*;
 public class MainController implements Initializable {
 
 
 
-    @FXML
-    private Button btnConvertisseur;
 
-    @FXML
-    private TextField imcResult;
 
     @FXML
-    private TextField txtPoids;
+    private VBox menuBiblio;
 
     @FXML
-    private TextField txtTaille;
+    private VBox menuConversion;
 
-    double taille = Double.parseDouble(txtPoids.getText());
-    double poids = Double.parseDouble(txtPoids.getText());
+    @FXML
+    private Button btnBiblio;
 
+    @FXML
+    private Button btnConversion;
 
+    @FXML
+    private Button btnImc;
 
+    @FXML
+    private VBox buttonBox;
+
+    @FXML
+    private VBox menuApp;
+
+    @FXML
+    private Button btnLibrary;
+
+    @FXML
+    private VBox mainMenu;
+
+    @FXML
+    private Button btnConv;
+
+    @FXML
+    private MenuItem menuItemMain;
+
+    @FXML
+    private MenuItem menuItemConv;
+
+    @FXML
+    private MenuItem menuItemBiblio;
+
+    @FXML
+    private MenuItem itemQuit;
+    @FXML
+    private Button btnQuit;
 
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+        menuApp.getChildren().removeAll( menuBiblio, menuConversion);
 
-
-
-        btnConvertisseur.setOnMouseClicked(btnConvertisseurAction -> {
-            System.out.println("marche");
-            double Result = poids / (taille * taille);
-            String Result2 = String.valueOf(Result);
-            imcResult.setText(Result2);
-
-
-
+        btnLibrary.setOnAction(event -> {
+            menuApp.getChildren().add(menuBiblio);
+            menuApp.getChildren().remove(mainMenu);
+        });
+        menuItemConv.setOnAction(event -> {
+            menuApp.getChildren().removeAll(menuBiblio,mainMenu);
+            menuApp.getChildren().add(menuConversion);
+        });
+        btnConv.setOnAction(event -> {
+            menuApp.getChildren().removeAll(mainMenu, menuBiblio);
+            menuApp.getChildren().add(menuConversion);
+        });
+        menuItemMain.setOnAction(event -> {
+            menuApp.getChildren().removeAll(menuConversion, menuBiblio);
+            menuApp.getChildren().add(mainMenu);
+        });
+        menuItemBiblio.setOnAction(event -> {
+            menuApp.getChildren().removeAll(menuConversion, mainMenu);
+            menuApp.getChildren().add(menuBiblio);
+        });
+        itemQuit.setOnAction(event -> {
+            System.exit(0);
+        });
+        btnQuit.setOnAction(event -> {
+            System.exit(0);
         });
 
-
-
     }
-
-
-
 }
-
