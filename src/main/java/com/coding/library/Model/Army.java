344fcab65b24813921087660d203cb6ae0e778a4
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2021-12-03 09:41:41 +0100

    Delete useless files

--- src/main/java/com/coding/library/Model/Army.java
+++ src/main/java/com/coding/library/Model/Army.java
@@ -10,7 +10,7 @@ public class Army {
     @Override
     public String toString() {
         return "Army{" +
-                "name='" + name + '\'' +
+                "name='" + getName() + '\'' +
                 '}';
     }
 
